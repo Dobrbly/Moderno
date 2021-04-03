93d97c88f5c8261ffdf7c565bce30fd6b4548a3c
Dobrbly <d.nekit@gmail.com>
2021-04-03 16:56:40 +0400

    added choice

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,16 @@
-$(function(){
-    var mixer = mixitup('.products__inner-box');
+$(function () {
+  var mixer = mixitup('.products__inner-box');
 
-    $(".rate__star").rateYo({
-        rating: 5,
-        readOnly: true,
-        starWidth: "11px"
-      });
+  $(".rate__star").rateYo({
+    rating: 5,
+    readOnly: true,
+    starWidth: "11px"
+  });
+
+  $('.products__slider-inner').slick({
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true
+  });
 });
